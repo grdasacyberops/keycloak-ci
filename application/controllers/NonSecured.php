21a82d6eea20c1a69eaffb7ebdcb26421892e995
dream-k <topdev.star@gmail.com>
2020-06-03 14:09:08 +0800

    test

--- application/controllers/NonSecured.php
+++ application/controllers/NonSecured.php
@@ -1,7 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class NonSecured extends CI_Controller {
-
+	public function __construct(){
+		parent::__construct();
+	}
 	/**
 	 * Index Page for this controller.
 	 *
